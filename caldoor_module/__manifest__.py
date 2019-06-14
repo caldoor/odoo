61af2ec3aea2bdbae2c9906a8d34f230c1f4c695
Joseph Serrano <josephsteve08@gmail.com>
2019-06-14 11:15:24 -0700

    customized res_partner - added fields and inherited view

--- caldoor_module/__manifest__.py
+++ caldoor_module/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "caldoor_module",
+    'name': "Cal Door Module",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Customization module solely for Cal Door & Drawer""",
 
     'description': """
-        Long description of module's purpose
+        ERP for Cal Door & Drawer
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "IT Support",
+    'website': "http://www.caldoor.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,13 +19,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale_management','account_accountant'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/res_partner_form_inherit.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
