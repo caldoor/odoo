1e6d1a401c3a2d114a2f62ec4fb4a70c4a6a0d75
P M <pme@odoo.com>
2020-01-16 14:55:19 +0530

    [FIX][2170994] caldoor_authorize: fix in '_refund_cleanup_lines'.

--- caldoor_authorize/models/account_invoice.py
+++ caldoor_authorize/models/account_invoice.py
@@ -8,5 +8,7 @@ class AccountInvoice(models.Model):
 
     @api.model
     def _refund_cleanup_lines(self, lines):
-        lines = lines.filtered(lambda l: not any([sol.is_cf_line for sol in l.sale_line_ids]))
+        # Don't know the actual reason behind this but it is calling from multiple places
+        # so it's just FIX
+        lines = lines.filtered(lambda l: l._name == 'account.invoice.line' and not any([sol.is_cf_line for sol in l.sale_line_ids]))
         return super(AccountInvoice, self)._refund_cleanup_lines(lines)
