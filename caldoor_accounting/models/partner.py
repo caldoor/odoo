58199f9fb750ca3502a9b09ca19facc563102d1c
Ali Alkaheli <aal@odoo.com>
2020-03-31 19:58:21 -0700

    [FIX] to refer certain form

--- caldoor_accounting/models/partner.py
+++ caldoor_accounting/models/partner.py
@@ -4,14 +4,14 @@ from odoo import models, api, _
 
 class Partner(models.Model):
     _inherit = "res.partner"
-
+##call super()
     @api.multi
     def name_get(self):
         res = []       
         for partner in self:
             if partner.name:                  
                 display_value = partner.name               
-                if partner.ref:
+                if partner.ref and self.env.context.get('show_attribute'):
                     display_value += ' (Cust# '                   
                     display_value += partner.ref 
                     display_value += ')'           
