50aa9c70dbf1405d916df42f922187d8647b3e22
Ana-Gabriel <agp@odoo.com>
2020-04-08 13:50:40 -0700

    [ADD] caldoor_account_reports: res_partner parent invoices

--- caldoor_account_reports/models/res_partner.py
+++ caldoor_account_reports/models/res_partner.py
@@ -5,10 +5,13 @@ from odoo.tools.misc import format_date
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    total_due = fields.Monetary(compute='_compute_for_followup', store=False, readonly=True)
+    def open_action_followup(self):
+        if self.parent_id:
+            return super(ResPartner, self.parent_id).open_action_followup()
+        return super(ResPartner, self).open_action_followup()
 
     def _compute_for_followup(self):
         for s in self.filtered(lambda x: x.parent_id):
-            print(s)
+            s.total_due = s.parent_id.total_due
         for s in self.filtered(lambda x: not x.parent_id):
             super(ResPartner, s)._compute_for_followup()
