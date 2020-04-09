c42320d9cc5529302b6f9c0386cd1336d7fc22af
Ana-Gabriel <agp@odoo.com>
2020-04-09 09:00:51 -0700

    [ADD] caldoor_account_reports: add related invoice partners to res model

--- caldoor_account_reports/models/res_partner.py
+++ caldoor_account_reports/models/res_partner.py
@@ -5,6 +5,33 @@ from odoo.tools.misc import format_date
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    due_invoice_partner_ids = fields.One2many(string='Other contacts', comodel_name='res.partner', compute='_compute_due_invoice_partner_ids')
+
+    def _compute_due_invoice_partner_ids(self):
+        Partner = self.env['res.partner']
+        Invoice = self.env['account.invoice']
+        for s in self:
+            ins = None
+            partners = None
+            contacts = []
+            
+            if s.parent_id:
+                partners = Partner.search(['|', ('parent_id', '=', s.parent_id.id), ('id', '=', s.parent_id.id)])
+                ins = Invoice.search([('partner_id', 'in', s.parts.ids)])
+            else:
+                partners = Partner.search(['|', ('parent_id', '=', s.id), ('id', '=', s.id)])
+                ins = Invoice.search([('partner_id', 'in', s.parts.ids)])
+            
+            if (not ins is None) and (not partners is None):
+                for i in ins:
+                    for p in partners:
+                        if i.partner_id.id == p.id:
+                            contacts.append(p.id)
+                
+                partners.filtered(lambda x: x.id in contacts)
+
+            s.due_invoice_partner_ids = partners
+
     def open_action_followup(self):
         if self.parent_id:
             return super(ResPartner, self.parent_id).open_action_followup()
