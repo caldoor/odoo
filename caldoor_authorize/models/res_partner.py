aaf82f761c4c0de4fc93cc7a4e2e3005f5b00d09
Zeno Nanon <zna@odoo.com>
2020-06-17 12:14:18 -0700

    [FIX] Change helper method to sort through authorized tokens

--- caldoor_authorize/models/res_partner.py
+++ caldoor_authorize/models/res_partner.py
@@ -25,11 +25,10 @@ class ResPartner(models.Model):
             return ','.join(sale_order_lines.mapped('order_id.name'))
         return ''
 
-    def _get_payment_tokens(self):
-        last_token = False
-        if len(self.payment_token_ids) > 0:
-            last_token = self.payment_token_ids[0]
-            for token in self.payment_token_ids[1:]:
-                if last_token.create_date < token.create_date:
-                    last_token = token
-        return last_token
+    def _get_payment_token(self):
+        payment_profile_id = False
+        if self.payment_token_ids:
+            authorize_tokens = self.payment_token_ids.filtered(lambda pt: pt.acquirer_id.provider == 'authorize')
+            if authorize_tokens:
+                payment_profile_id = authorize_tokens.sorted(key='create_date desc')[0].authorize_profile
+        return payment_profile_id
