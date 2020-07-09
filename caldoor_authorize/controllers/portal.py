e9462437e40362e88a3792f6c36de9192a61db3c
Zeno Nanon <zna@odoo.com>
2020-07-09 14:33:25 -0700

    [ADD] controller to redirect with payment token updated

--- caldoor_authorize/controllers/portal.py
+++ caldoor_authorize/controllers/portal.py
@@ -3,6 +3,7 @@
 from odoo import http
 from odoo.http import request
 from odoo.addons.payment.controllers.portal import WebsitePayment
+import datetime
 
 class WebsitePayment(WebsitePayment):
     @http.route(['/partner/<int:p_id>/<int:record_id>/payment_method/'], type='http', auth="user", website=True)
@@ -14,7 +15,8 @@ class WebsitePayment(WebsitePayment):
         partner = request.env["res.partner"].browse([p_id])
         payment_tokens = partner.payment_token_ids
         payment_tokens |= partner.commercial_partner_id.sudo().payment_token_ids
-        return_url = request.params.get('redirect', '/web#id={}&action=148&model=account.payment&view_type=form&menu_id=175'.format(record_id))
+        return_url = request.params.get('redirect', '/partner/{}/{}/process_payment/'.format(p_id, record_id))
+        
         values = {
             'pms': payment_tokens,
             'acquirers': acquirers,
@@ -24,3 +26,17 @@ class WebsitePayment(WebsitePayment):
             'partner_id': partner.id
         }
         return request.render("caldoor_authorize.pay_methods", values)
+
+    @http.route(['/partner/<int:p_id>/<int:record_id>/process_payment/'], type='http', auth="user", website=True)
+    def partner_process_payment(self, p_id, record_id, **kwargs):
+        record = request.env['account.payment'].browse([record_id])
+        partner = request.env['res.partner'].browse([p_id])
+        payment_tokens = partner.payment_token_ids
+        payment_tokens |= partner.commercial_partner_id.sudo().payment_token_ids
+        latest_token = payment_tokens[0] | None
+        for token in payment_tokens[1:]:
+            if latest_token.create_date == datetime.datetime.now().date():
+                latest_token = token
+        if latest_token != None:
+            record.update({'payment_token_id': latest_token.id})
+        return request.redirect('/web#id={}&action=148&model=account.payment&view_type=form&menu_id=175'.format(record_id))
