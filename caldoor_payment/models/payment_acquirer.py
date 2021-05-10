18bdd9d84974a2f514eaedcd13a2b911d0d58170
Kent Kawahara <kak@odoo.com>
2021-05-10 16:53:00 -0700

    [FIX] caldoor_payment: fix datetime formatting issue

--- caldoor_payment/models/payment_acquirer.py
+++ caldoor_payment/models/payment_acquirer.py
@@ -13,13 +13,12 @@ class PaymentToken(models.Model):
 
     @api.depends('payment_ids')
     def _compute_last_successful_transaction_date(self):
-        timezone = tz.gettz('America/Los_Angeles')
         for token in self:
             last_date = None
             for payment in token.payment_ids:
                 if payment.state == 'done' and (not last_date or payment.date > last_date):
                     last_date = payment.date
-            token.last_successful_transaction_date = last_date.astimezone(timezone) if last_date else None
+            token.last_successful_transaction_date = last_date
 
     @api.multi
     def name_get(self):
@@ -29,7 +28,13 @@ class PaymentToken(models.Model):
             if token.contact_name:
                 token_name += ' %s' % token.contact_name
             if token.last_successful_transaction_date:
-                date_str = token.last_successful_transaction_date.strftime('%m/%d/%Y %I:%M%p')
-                token_name += ' %s' % date_str
+
+                token_name += ' %s' % token._get_formatted_time()
             names.append((token.id, token_name))
         return names
+
+    def _get_formatted_time(self):
+        self.ensure_one()
+        timezone = tz.gettz('America/Los_Angeles')
+        transaction_date_pacific = self.last_successful_transaction_date.astimezone(timezone)
+        return transaction_date_pacific.strftime('%m/%d/%Y %I:%M%p')
