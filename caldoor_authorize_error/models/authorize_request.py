0e297c71605f48abb456ee1140cd1b9a3fd66e04
Sri Sai Bhargav Koritala(sbk) <sbk@odoo.com>
2021-06-04 00:16:42 +0000

    [MODIFY] change authorize request to modify reponse for error checking

--- caldoor_authorize_error/models/authorize_request.py
+++ caldoor_authorize_error/models/authorize_request.py
@@ -5,34 +5,35 @@ from odoo.addons.payment_authorize.models.authorize_request import AuthorizeAPI
 
 class AuthorizeAPI(AuthorizeAPI):
     
-    def error_check(elem):
-        """Check if the response sent by Authorize.net contains an error.
-        Errors can be a failure to try the transaction (in that case, the transasctionResponse
-        is empty, and the meaningful error message will be in message/code) or a failure to process
-        the transaction (in that case, the message/code content will be generic and the actual error
-        message is in transactionResponse/errors/error/errorText).
-        :param etree._Element elem: the root element of the response that will be parsed
-        :rtype: tuple (bool, str)
-        :return: tuple containnig a boolean indicating if the response should be considered
-                 as an error and the most meaningful error message found in it.
+    def _authorize_request(self, data):
+        """Encode, send and process the request to the Authorize.net API.
+        Encodes the xml data and process the response. Note that only a basic
+        processing is done at this level (namespace cleanup, basic error management).
+        :param etree._Element data: etree data to process
         """
-        result_code = elem.find('messages/resultCode')
-        msg = 'No meaningful error message found, please check logs or the Authorize.net backend'
-        
-        #-----------------custom code starts here---------------------
-        
-        transaction_errors = elem.find('transactionResponse/errors')
-        has_error = result_code.text == 'Error' or transaction_errors is not None
-        
-        #-----------------custom code ends here-----------------------
+        logged_data = data
+        data = etree.tostring(data, encoding='utf-8')
+        for node_to_remove in ['//merchantAuthentication', '//creditCard']:
+            for node in logged_data.xpath(node_to_remove):
+                node.getparent().remove(node)
+        logged_data = str(etree.tostring(logged_data, encoding='utf-8', pretty_print=True)).replace(r'\n', '\n')
+        _logger.info('_authorize_request: Sending values to URL %s, values:\n%s', self.url, logged_data)
 
-        if has_error:
-            # accumulate the most meangingful error
-            error = elem.find('transactionResponse/errors/error')
-            error = error if error is not None else elem.find('messages/message')
-            if error is not None:
-                code = error[0].text
-                text = error[1].text
-                msg = '%s: %s' % (code, text)
-        return (has_error, msg)
+        r = requests.post(self.url, data=data, headers={'Content-Type': 'text/xml'})
+        r.raise_for_status()
+        response = strip_ns(r.content, XMLNS)
 
+        logged_data = etree.XML(r.content)
+        logged_data = str(etree.tostring(logged_data, encoding='utf-8', pretty_print=True)).replace(r'\n', '\n')
+        _logger.info('_authorize_request: Values received\n%s', logged_data)
+        
+        #-------------custom code starts here---------------
+        
+        #if transacation response has errors, then resultcode type is changed to error.
+        transaction_errors = response.find('transactionResponse/errors')
+        if transaction_errors is not None:
+            response.find('messages/resultCode').text = 'Error'
+        
+        #-------------custom code ends here------------------
+        
+        return response
